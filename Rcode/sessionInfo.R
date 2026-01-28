cedb61015db4b55042a2bbd461ff4575b2305a6a
Hyeondo-Jeon <previous_year@khu.ac.kr>
2026-01-28 20:29:04 +0900

    Create sessionInfo.R

---
+++ Rcode/sessionInfo.R
@@ -0,0 +1,14 @@
+# sessionInfo()
+# R version 4.3.0 (2023-04-21)
+# Platform: x86_64-pc-linux-gnu (64-bit)
+# Running under: Ubuntu 20.04.6 LTS
+# 
+# other attached packages:
+# [1] pheatmap_1.0.12             viridis_0.6.5               viridisLite_0.4.2             TrajectoryUtils_1.8.0      
+# [6] princurve_2.1.6             scDblFinder_1.14.0          SingleCellExperiment_1.24.0 SummarizedExperiment_1.32.0 Biobase_2.62.0             
+# [11] GenomicRanges_1.52.1        GenomeInfoDb_1.36.4         IRanges_2.34.1              S4Vectors_0.40.2            BiocGenerics_0.48.1        
+# [16] MatrixGenerics_1.14.0       matrixStats_1.5.0           DoubletFinder_2.0.6         scales_1.4.0                readxl_1.4.3               
+# [21] rbioapi_0.8.1               DGEobj.utils_1.0.6          umap_0.2.10.0               circlize_0.4.16             ComplexHeatmap_2.21.2      
+# [26] lubridate_1.9.4             forcats_1.0.0               stringr_1.5.1               dplyr_1.1.4                 purrr_1.0.4                
+# [31] readr_2.1.4                 tidyr_1.3.1                 tibble_3.2.1                ggplot2_3.4.4               tidyverse_2.0.0            
+# [36] Seurat_5.3.0                SeuratObject_5.0.2          sp_2.1-4
